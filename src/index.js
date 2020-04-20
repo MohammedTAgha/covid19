d03139050b324586955d663ea1527a1f8a78a95c
MOHAMMETDAGHA <36562531+MOHAMMETDAGHA@users.noreply.github.com>
2020-04-21 02:19:17 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+export {default as Cards } from './Cards/Cards'
+export {default as Chart } from './Chart/Charts'
+export {default as Countries } from './Countries/Countries'
+export {default as Landing } from './Landing/Landing'
+export {default as Navigation } from './Landing/Navigation'
